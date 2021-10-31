62ab9d6abd94c6738234c0ce5b8a7423fdddfb25
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-10-31 19:21:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+let tabs = document.querySelectorAll(".times li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".info div");
+let divsArray = Array.from(divs);
 
+tabsArray.forEach((ele) => {
+    ele.addEventListener("click", function (e) {
+        tabsArray.forEach((ele) => {
+            ele.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        divsArray.forEach((div) => {
+            div.style.display = "none";
+        });
+        let info = document.querySelectorAll(`.${e.currentTarget.dataset.cont}`);
+        info.forEach((ele) => {
+            ele.style.display = 'block';
+        })
+    });
+});
